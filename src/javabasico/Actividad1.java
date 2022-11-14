f54da4996fd6b9fd763d145b6cdf040b32a704a5
Ingrid Fonoy <ingridsfonoyd@gmail.com>
2022-11-14 11:31:35 -0600

    act1and2

--- src/javabasico/Actividad1.java
+++ src/javabasico/Actividad1.java
@@ -3,9 +3,14 @@ package javabasico;
 public class Actividad1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola Actividad 1");
-
+		/*Crea un programa donde definamos las siguientes variables y las convirtamos a
+		los distintos tipos indicados, mostrando el resultado de la conversión por consola.
+		int a = 65; → long y char
+		long b = 10 → double y int
+		char c = ‘A’ → int
+		double d = 1.8 → float y int*/
+		
+		
 	}
 
 }
---
+++ src/javabasico/Actividad2.java
@@ -0,0 +1,30 @@
+package javabasico;
+
+import java.util.Scanner;
+
+public class Actividad2 {
+
+	public static void main(String[] args) {
+		/*Hacer un programa, donde se definirán tres variables locales nombre, apellidos y
+		edad, siendo esta última de tipo entero. Estas variables se introducirán por consola.
+		Finalmente mostrará un mensaje en consola usando las variables de la siguiente forma: “Hola
+		nombre apellidos tienes edad años*/
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		int nombre;
+		int apellido;
+		int edad;
+		
+		System.out.println("Nombre(s)");
+		nombre = teclado.nextInt();
+		
+		System.out.println("Apellido(s)");
+		apellido = teclado.nextInt();
+		
+		System.out.println("Edad");
+		edad = teclado.nextInt();
+
+	}
+
+}
